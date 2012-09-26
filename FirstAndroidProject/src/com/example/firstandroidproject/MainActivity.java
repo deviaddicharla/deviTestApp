b98600b7c559f1e88edf9d6a1449bb60070e2be2
Devi Addicharla <devi@divi.co.in>
2012-09-26 14:48:24 +0530

    First Commi

---
+++ FirstAndroidProject/src/com/example/firstandroidproject/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.firstandroidproject;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        System.out.println();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+}
