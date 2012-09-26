c6ef2c9fb85ecd5fc0e423bb70b3a50ebd98922a
Devi Addicharla <devi@divi.co.in>
2012-09-26 16:18:20 +0530

    Added the System.out.printl

--- FirstAndroidProject/src/com/example/firstandroidproject/MainActivity.java
+++ FirstAndroidProject/src/com/example/firstandroidproject/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
-        System.out.println();
+        System.out.println("********");
     }
 
     @Override
